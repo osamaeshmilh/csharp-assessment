ce163ad8eda92649299bde87c47736b091059820
osamaeshmilh <osamaeshmilh@gmail.com>
2022-11-04 13:04:22 +0000

    code fo binary to integer convertion

--- Binary/Binary.cs
+++ Binary/Binary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace CP
 {
@@ -12,20 +13,34 @@ namespace CP
     class Binary
     {
 
-        Int32 X
+        Int16 X;
+        Int16[] binaryArray = {0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0};
+        string binaryNumber = "";
+        public static implicit operator Binary(Int16 value)
         {
-            get;
-            set;
-        }
-
-        public static implicit operator Binary(Int32 value)
-        {
+            //Console.WriteLine($"{value});
+            Boolean isNegative = false;
+            string binaryNumber = Convert.ToString(value, 2);
+            Console.WriteLine(binaryNumber);
+            if(value < 0)
+            {
+                isNegative = true;
+            }
+            int sum = 0;
+            int counter = 0;
+            //isNegative ? 16 : binaryNumber.Length - 1;
+            for (int i = binaryNumber.Length - 1; i >= 0; i--)
+            {
+                sum = sum + (int.Parse(binaryNumber[i].ToString()) * Convert.ToInt16(Math.Pow(2, counter)));
+                counter++;
+            }
+            Console.WriteLine($"sum is: {sum}");
             return new Binary()
             {
                 X = value
             };
         }
-
+        
         #region(Fields)
         #endregion
         #region(Properties)
